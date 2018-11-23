2684f125a8359f07241813f80719d11bce3cbef3
multaq <44582981+multaq@users.noreply.github.com>
2018-11-23 16:59:57 +0300

    Update clikea.js

--- clikea.js
+++ clikea.js
@@ -1,15 +1,1 @@
-function clickerf(){
-    var element = document.getElementById("div1");
-    if ("click" in element){
-                element.click();
-    }else if ("dispatchEvent" in element) {
-     
-        var evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent("click", true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        element.dispatchEvent(evt);
-    }
- 
-}
-
-
-setTimeout(clickerf,5000);
+var scroll = setInterval(function(){ window.scrollBy(0,1000); }, 2000);
